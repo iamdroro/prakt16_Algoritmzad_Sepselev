fb50a0a84af805ae4fa9e6859a74491263ceb1b9
iamdroro <123649623+iamdroro@users.noreply.github.com>
2023-05-02 21:15:36 +0500

    Add files via upload

---
+++ pr16_1_SepselevTim/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Prakt16Algoritm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(310, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбор файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(665, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите слово, для нахождения количества вхождений";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(71, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 84);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(310, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 74);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Найти кол-во вхождений";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Prakt16Algoritm.Properties.Resources._1463122060_5;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pr16_1_SepselevTim/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prakt16Algoritm
+{
+    public partial class Form1 : Form
+    {
+
+        string Readtxt;
+        int A = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                Readtxt = File.ReadAllText(file);
+                button1.Visible = false;
+                panel1.Visible = true;
+                button2.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Файл не был выбран!");
+            }
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string LT="";
+            A = 0;
+            for (int i = 0; i < Readtxt.Length; i++)
+            {
+                if (!char.IsPunctuation(Readtxt[i]))
+                {
+                    LT += Readtxt[i];
+                }
+            }
+            string[] txt = LT.Split(' ');
+            foreach (string txtone in txt)
+            {
+                string textbox = textBox1.Text.ToLower();
+                if (txtone.ToLower() == textbox)
+                {
+                    A++;
+                }
+            }
+            if (A>0)
+            MessageBox.Show($"В тексте {Readtxt} было найдено {A} вхождений слова {textBox1.Text}", "Сообщение", MessageBoxButtons.OK);
+            else MessageBox.Show($"Вхождений не было найдено", "Сообщение", MessageBoxButtons.OK);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
