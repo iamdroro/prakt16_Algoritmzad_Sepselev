d127386d6c1a3505edafb2ddffaccdda2e4994b9
iamdroro <123649623+iamdroro@users.noreply.github.com>
2023-05-05 11:19:20 +0500

    Add files via upload

---
+++ pract16_3_sepselev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract16_3_sepselev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Сколько элементов вводится в массив? ");
+                int a = int.Parse(Console.ReadLine());
+                double[] gt = new double[a];
+                for (int i = 0; i < gt.Length; i++)
+                {
+                    Console.WriteLine($"Введите {i + 1} элемент ");
+                    gt[i] = double.Parse(Console.ReadLine());
+                }
+                var n = gt.GroupBy(x => x).ToDictionary(b => b.Key, b => b.Count());
+                Console.WriteLine("Частота элементов в массиве ");
+                foreach (var juj in n)
+                {
+                    Console.WriteLine("{0}-{1}", juj.Key, juj.Value);
+                }
+                Console.WriteLine("число * на его частоту ");
+                foreach (var juj in n)
+                {
+                    Console.WriteLine("{0}-{1}", juj.Key * juj.Value, juj.Value);
+                }
+            }
+            catch { Console.WriteLine("You have a little misstake in your writing"); }
+            Console.ReadLine();
+        }
+       
+    }
+}
