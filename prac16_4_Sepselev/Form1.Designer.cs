7e003f6db2251f9c721503f3929ff7c1a36caf6c
iamdroro <123649623+iamdroro@users.noreply.github.com>
2023-05-08 20:29:33 +0500

    Add files via upload

---
+++ prac16_4_Sepselev/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace prac16zad4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palace Script MT", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(319, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отсортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.YellowGreen;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(255, 190);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(266, 225);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите число по которому нужно отсортировать численность страны";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::prac16zad4.Properties.Resources._1613627651_35_p_fon_dlya_prezentatsii_derevo_53;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ prac16_4_Sepselev/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prac16zad4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                
+                if (long.Parse(textBox1.Text) > 0)
+                {
+                    Dictionary<string, long> podschet = new Dictionary<string, long>();
+                    using (StreamReader rx = new StreamReader("r1.txt"))
+                    {
+                        string chtenie;
+                        while ((chtenie = rx.ReadLine()) != null)
+                        {                           
+                            string[] strana = chtenie.Split(' ');
+                            string country = strana[0];
+                            if (strana.Length == 4)
+                            {
+                                long numeric = long.Parse($"{strana[1]}{strana[2]}{strana[3]}");
+                                podschet[country] = numeric;
+                            }
+                            else if (strana.Length == 5)
+                            {
+                                long numeric = long.Parse($"{strana[1]}{strana[2]}{strana[3]}{strana[4]}");
+                                podschet[country] = numeric;
+                            }
+                        }
+                    }
+                    var sort = podschet.Where(a => a.Value > long.Parse(textBox1.Text)).OrderBy(a => a.Key.Length).ThenBy(a => a.Key);
+                    foreach (var yt in sort)
+                    {
+                        listBox1.Items.Add($"{yt.Key} {yt.Value}");
+                    }
+                }
+                else MessageBox.Show("Вы ввели отрицательное число", "Сообщение", MessageBoxButtons.OK);
+            }
+            catch { MessageBox.Show("Введите число правельно!", "Сообщение", MessageBoxButtons.OK); }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
